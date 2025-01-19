d54f3df4fc9ae30006f653cb69fd50f0ce8c839b
psz2007 <panshizhao2007@163.com>
2025-01-19 22:49:32 +0800

    better display / add help & settings

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ const initdata =
 ["反方四辩","tmr",240,""] ["正方四辩","tmr",240,""]
 ["投票环节","tmr",60,""]
 ["最终得分","cnt",2,""]`;
-let data = initdata;
+let data = initdata, ts = [];
 
 function parse(s) {
+	document.getElementById("main").innerHTML = "";
 	let l = s.split("\n"), info = [];
+	ts = [];
 	for (let i in l) {
 		const tl = l[i].split(" "), n = tl.length;
 		if (n == 0) {
@@ -22,11 +24,11 @@ function parse(s) {
 			let t = document.createElement("div"), val = JSON.parse(tl[j]);
 			t.className = "column";
 			if (val[1] == "cnt") {
-				t.innerHTML = `
-				<div id="${i}-${j}">
-					<div class="title">${val[0] + " " + val[3]}</div>
+				t.innerHTML =
+				`<div id="${i}_${j}" class="ui segment">
+					<div class="title" id="${i}_${j}_title">${val[0]}</div>
 					<br/>
-					<div id="${i}-${j}-prog" class="ui multiple progress">
+					<div id="${i}_${j}_prog" class="ui multiple progress">
 						<div class="blue bar">
 							<div class="progress"></div>
 						</div>
@@ -37,39 +39,83 @@ function parse(s) {
 					</div>
 				</div>`
 			} else {
-				t.innerHTML = `
-				<div id="${i}-${j}">
-					<div class="title">${val[0] + val[3]}</div>
+				t.innerHTML = 
+				`<div id="${i}_${j}" class="ui segment">
+					<div class="title" id="${i}_${j}_title">${val[0] + " " + val[3]}</div>
 					<br/>
-					<p id="${i}-${j}-timer" class="timer"></p>
+					<p id="${i}_${j}_timer" class="timer"></p>
 					</div>
 				</div>`;
 			}
 			cur.appendChild(t);
-			val.push(`${i}-${j}`);
+			val.push(`${i}_${j}`);
 			info.push(val);
+			if (val[1] == "tmr") {
+				let v = document.createElement("div");
+				v.className = "ui labeled small input";
+				v.innerHTML += 
+				`<div class="ui label">${val[0]}</div>
+				<input name="${i}_${j}_set" type="text">`;
+				document.getElementById("names").appendChild(v);
+				ts.push([val[0], `${i}_${j}`]);
+			}
 		}
 		document.getElementById("main").appendChild(cur);
 	}
+	document.getElementById("names").innerHTML +=
+	`<button class="ui primary button" onclick="_input()">填入</button>`;
 	return info;
 }
 
 function getcol(s) {
 	if (s[0] == "正") {
 		return "red";
-	} else if(s[0] == "反") {
+	} else if (s[0] == "反") {
 		return "blue";
 	} else {
 		return "grey";
 	}
 }
-function activate(x, col) {
+function inv(s) {
+	if (s == "red") {
+		return "blue";
+	} else if (s == "blue") {
+		return "red";
+	} else {
+		return "black";
+	}
+}
+function activate(x, ty, col) {
 	const cur = document.getElementById(x);
-	cur.style = "padding: 5px; border-radius: 5px; border: 2px solid "+col;
+	cur.className = `ui ${col} tertiary inverted segment`;
+	let t = document.createElement("div");
+	t.style = "text-align: center";
+	if (ty == "tmr") {
+		t.innerHTML = 
+		`<button class="ui button" onclick="_pau()">停止</button>
+		<button class="ui primary button" onclick="_nxt()">下一步</button>
+		<div id="${x}_timer_prog" class="ui tiny attached progress">
+			<div class="${inv(col)} bar">
+				<div class="progress"></div>
+			</div>
+		</div>`;
+	} else {
+		t.innerHTML = 
+		`<div class="ui input">
+			<input id="pos" type="text" placeholder="正方">
+		</div>
+		<div class="ui input">
+			<input id="neg" type="text" placeholder="反方">
+		</div>
+		<button class="ui button" onclick="_chr()">确认</button>
+		<button class="ui primary button" onclick="_nxt()">下一步</button>`;
+	}
+	cur.appendChild(t);
 }
 function deactivate(x) {
 	const cur = document.getElementById(x);
-	cur.style = "";
+	cur.className = "ui segment";
+	cur.removeChild(cur.lastChild);
 }
 function formatDate(s, fmt = "yyyy 年 MM 月 dd 日 hh 时 mm 分 ss 秒 SS") {
 	if (s == "")
@@ -100,13 +146,13 @@ function formatDate(s, fmt = "yyyy 年 MM 月 dd 日 hh 时 mm 分 ss 秒 SS") {
 
 let flg1 = false, flg2 = false, flg3 = false;
 function _nxt() {
-	flg1 = true;
+	flg1 = true; flg2 = flg3 = false;
 }
 function _chr() {
-	flg2 = true;
+	flg2 = true; flg1 = false;
 }
 function _pau() {
-	flg3 = true;
+	flg3 = true; flg1 = false;
 }
 
 function until(c) {
@@ -124,45 +170,66 @@ async function runTimer(w) {
 		if (t > lim * 1000) {
 			document.getElementById(x).style.color = "red";
 		}
+		$('.ui.tiny.attached.progress').progress('set percent', Math.min(t / lim / 10, 100.));
 		if (flg3) {
 			clearInterval(id);
 		}
-	}, 10, w[4] + "-timer", w[2]);
+	}, 10, w[4] + "_timer", w[2]);
 	await until(() => flg3);
 	await until(() => flg1);
 	return id;
 }
 async function runScore(x) {
 	await until(() => flg2);
-	let v0 = parseInt(document.getElementById("pos").value), v1 = parseInt(document.getElementById("neg").value);
-	$("#" + x + "-prog").progress("reset");
-	$("#" + x + "-prog").progress("set total", v0 + v1);
-	$("#" + x + "-prog").progress("set progress", [v0, v1]);
-	$("#" + x + "-prog").progress({
+	let v0 = parseInt(document.getElementById("pos").value), v1 = parseInt(document.getElementById("neg").value), id = `#${x}_prog`;
+	$(id).progress("reset");
+	$(id).progress("set total", v0 + v1);
+	$(id).progress("set progress", [v0, v1]);
+	$(id).progress({
 		text: {
 			percent: '{bar}:{percent}%',
 			bars: ['正方', '反方']
 		}
 	});
-	$("#" + x + "-prog").progress("set label", `正方：${v0} —— 反方:${v1}`);
+	$(id).progress("set label", `正方：${v0} —— 反方:${v1}`);
 	await until(() => flg1);
 }
 
-async function _pageBuild(){
+async function _pageBuild() {
+	$('#help').sidebar({
+		silent: true
+	});
+	$('#settings').sidebar({
+		silent: true
+	});
 	const info = parse(data);
 	for (let i in info) {
 		if (info[i][1] == "tmr") {
-			document.getElementById(info[i][4] + "-timer").innerText = "00:00.00";
+			document.getElementById(info[i][4] + "_timer").innerText = "00:00.00";
 		}
 	}
 	for (let i in info) {
-		flg1 = flg2 = flg3 = false;
-		activate(info[i][4], getcol(info[i][0]));
+		flg1 = false;
+		activate(info[i][4], info[i][1], getcol(info[i][0]));
 		if (info[i][1] == "tmr") {
 			await runTimer(info[i]);
 		} else {
 			await runScore(info[i][4]);
 		}
 		deactivate(info[i][4]);
+	}
+}
+
+function _showHelp() {
+	$('#help').sidebar('toggle');
+}
+
+function _showSettings() {
+	$('#settings').sidebar('toggle');
+}
+
+function _input() {
+	for (let i in ts) {
+		document.getElementById(ts[i][1] + "_title").innerText = ts[i][0] + " " + document.querySelector(`input[name='${ts[i][1]}_set']`).value;
 	}
 }
